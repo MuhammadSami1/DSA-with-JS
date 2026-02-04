d5d088d87d667241f7ee52a489c5334194cd20a5
Muhammad Sami <muhammadsami1242@gmail.com>
2026-02-04 18:44:09 +0500

    add more solutions'

---
+++ Easy/2026-02-3-0001-TwoSum/solution.ts
@@ -0,0 +1,15 @@
+function twoSumTs(nums: number[], target: number) {
+  var map = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+    if (map.has(complement)) {
+      return [map.get(complement), i];
+    }
+    map.set(nums[i], i);
+  }
+
+  return [];
+}
+
+console.log(twoSumTs([2, 7, 11, 15], 9));
